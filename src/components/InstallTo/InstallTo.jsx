16f1cf8c3e1207b4bb1cf57184b1e1c708989a6f
Marco Ramirez <mvrcentes@hotmail.com>
2023-03-09 20:02:02 -0600

    Add "add duckduckgo" button

--- src/components/InstallTo/InstallTo.jsx
+++ src/components/InstallTo/InstallTo.jsx
@@ -1,11 +1,15 @@
+import './InstallTo.css'
+
 const InstallTo = () => {
     return (
         <div className="InstallTo">
-            <h1>Tired of being tracked online? We can help</h1>
+            <h1 className="title">Tired of being tracked online? We can help</h1>
             <p>We don't store your personal info. We don't follow you around with ads.</p>
             <p>We don't track you. Ever.</p>
-            <button type="button" className="button">Add DuckDuckGo to Safari</button>
-            <p>Trusted by tens of millions worldwide!</p>
+            <div className="buttonContainer">
+                <button type="button" className="addTo">Add DuckDuckGo to Safari</button>
+            </div>
+            <p className="advice">Trusted by tens of millions worldwide!</p>
         </div>
     );
 }
