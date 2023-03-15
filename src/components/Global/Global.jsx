80e403584969ae77b1ae44aa3c6b7ad04ce1d04c
Fabian Juarez <83832041+FabianJuarez182@users.noreply.github.com>
2023-03-14 18:17:26 -0600

    Header finished

--- src/components/Global/Global.jsx
+++ src/components/Global/Global.jsx
@@ -1,9 +1,11 @@
 import InstallTo from '../InstallTo/InstallTo.jsx'
 import SearchBar from '../SearchBar/SearchBar.jsx'
+import Header from '../Header/Header.jsx'
 
 const Global = () => {
     return (
         <div>
+            <Header />
             <SearchBar />
             <InstallTo/>
         </div>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,13 @@
+import './Header.css';
+
 const Header = () => {
-    
+    return(
+        <div className="header-wrapper">
+        <img className="Megaphone-icon" src="../src/assets/megaphone.png"/>
+        <img className="popout-trig" src="../src/assets/arrow.png"/>
+        <img className="header__button--menu" src="../src/assets/menu.png"/>
+        </div>
+    )
 }
 
 export default Header
